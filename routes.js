dc1327e9a8d78eb7d15caf32af734bcec8e5d348
Dean Slama <deanslamajr@gmail.com>
2015-10-29 10:39:00 -0500

    added router logic for /contest

--- routes.js
+++ routes.js
@@ -10,4 +10,8 @@ router.get('/members', function (req, res){
   res.render('members');
 });
 
+router.get('/contest', function (req, res) {
+	res.redirect(301, 'http://goo.gl/forms/V03Q04bj1G');
+});
+
 module.exports = router;
