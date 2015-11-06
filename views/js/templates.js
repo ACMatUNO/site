3e97454ec2bc11c364e09f8ea5ace8cb63ed359a
ddeker <dldekerl@uno.edu>
2015-11-06 22:38:21 +0000

    redesigned index page and navbar

--- views/js/templates.js
+++ views/js/templates.js
@@ -2,17 +2,18 @@
 var NavBar = React.createClass({
   render: function() {
     return (
-      <div className = "top_menu">
-         <div className = "container">
-            <ul className = "pull-left">
-               <li><a href="../">Home</a></li>
+      <nav className = "navbar navbar-default navbar-fixed-top">
+        <div className = "container-fluid" id="navigation">
+            <div className ="navbar-header">
+              <a className="navbar-brand" href="../"><img src="/img/unoiconsmall.png" /></a>
+            </div>
+            <ul className ="nav navbar-nav navbar-right">
+               <li><a href="/news">Events <span className="glyphicon glyphicon-calendar"></span></a></li>
+               <li><a href="/members">Current Members <span className="glyphicon glyphicon-user"></span></a></li>
+               <li><a href="#">Login <span className="glyphicon glyphicon-log-in"></span></a></li>
             </ul>
-            <ul className = "pull-right">
-               <li><a href="/members">Current Members</a></li>
-               <li><a href="#">Login</a></li>
-            </ul>
-         </div>
-      </div>
+        </div>
+      </nav>
     );
   }
 });
